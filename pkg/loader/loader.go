e3539a32616902a49858a9d649ea45443775303b
Ермолов Алексей Геннадьевич <agermolov@mts.ru>
2024-07-09 12:20:53 +0400

    implement app

---
+++ pkg/loader/loader.go
@@ -0,0 +1,118 @@
+package loader
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/beevik/guid"
+)
+
+type Loader struct {
+}
+
+type Payload struct {
+	Substitution map[string]interface{} `json:"substitution"`
+	Template     map[string]interface{} `json:"template"`
+}
+
+type Settings struct {
+	BootstrapServers string `json:"bootstrapServers"`
+	Topic            string `json:"topic"`
+	ClientId         string `json:"clientId"`
+	Partition        int    `json:"partition"`
+}
+
+type Message struct {
+	Settings *Settings
+	Message  bytes.Buffer
+}
+
+func GetSettings() *Message {
+	config := flag.String("config", "./config.json", "settings file path")
+	payload := flag.String("payload", "./payload.json", "Kafka payload template")
+
+	flag.Parse()
+
+	loader := &Loader{}
+	parsed := loader.ParseSettings(*config, *payload)
+
+	return parsed
+}
+
+func (l *Loader) ParseSettings(config string, payload string) *Message {
+	// config
+	settings := l.LoadSettings(config)
+
+	// payload
+	load := l.LoadPayload(payload)
+
+	var result bytes.Buffer
+	resultSub := l.BuildSub(load.Substitution)
+
+	str, _ := json.Marshal(load.Template)
+	t := template.Must(template.New("").Parse(string(str[:])))
+
+	if err := t.Execute(&result, resultSub); err != nil {
+		panic(err)
+	}
+
+	log.Default().Println()
+	log.Default().Println()
+	log.Default().Println("Message was:")
+	log.Default().Println(result.String())
+	log.Default().Println()
+	log.Default().Println()
+
+	return &Message{
+		Settings: &settings,
+		Message:  result,
+	}
+}
+
+func (l *Loader) LoadSettings(config string) Settings {
+	var settings Settings
+	configFile, err := os.Open(config)
+	if err != nil {
+		log.Fatalln("Cannot open settings file")
+	}
+	defer configFile.Close()
+
+	byteValue, _ := io.ReadAll(configFile)
+	json.Unmarshal(byteValue, &settings)
+
+	return settings
+}
+
+func (l *Loader) LoadPayload(payload string) Payload {
+	var load Payload
+	payloadFile, err := os.Open(payload)
+	if err != nil {
+		log.Fatalln("Cannot open settings file")
+	}
+	defer payloadFile.Close()
+
+	payl, _ := io.ReadAll(payloadFile)
+	json.Unmarshal(payl, &load)
+
+	return load
+}
+
+func (l *Loader) BuildSub(sub map[string]interface{}) map[string]interface{} {
+	for key, val := range sub {
+		if val.(string) == "{{@guid}}" {
+			sub[key] = guid.New().String()
+		}
+
+		if val.(string) == "{{@now}}" {
+			sub[key] = time.Now().Format(time.RFC3339)
+		}
+	}
+
+	return sub
+}
---
+++ pkg/processor/processor.go
@@ -0,0 +1,45 @@
+package processor
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/alexermolov/go-kafka-pusher/pkg/loader"
+	"github.com/segmentio/kafka-go"
+)
+
+type Processor struct {
+	Message *loader.Message
+}
+
+func NewProcessor(settings *loader.Message) *Processor {
+	return &Processor{
+		Message: settings,
+	}
+}
+
+func (proc *Processor) Push() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", proc.Message.Settings.BootstrapServers, proc.Message.Settings.Topic, proc.Message.Settings.Partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: proc.Message.Message.Bytes()},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	log.Default().Println()
+	log.Default().Println()
+	log.Default().Printf("Message sent to %s topic %s partition %d", proc.Message.Settings.BootstrapServers, proc.Message.Settings.Topic, proc.Message.Settings.Partition)
+	log.Default().Println()
+	log.Default().Println()
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
